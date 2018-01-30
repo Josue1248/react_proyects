b6a805472df842c6d0c73543d68d0d6dbfb56347
josue1248 <josuem.inf@gmail.com>
2018-01-30 08:31:35 -0800

    En clase

---
+++ indecision-app/public/scripts/app.js
@@ -0,0 +1,7 @@
+console.log("App.js esta corriendo");
+
+//JSX JavaScript XML
+//var template =<p>Esto es JSX desde app.js</p>
+
+var appRoot=document.getElementById("app");
+ReactDom.render(template,appRoot);
